75e1b477afeb15c33d303d6eee264fb12fe7111d
anaboyshchikov <naboyshchikov@gmail.com>
2017-11-02 13:06:41 +0300

    add sample; draw tolerance intervals

--- src/components/ToleranceInterval.jsx
+++ src/components/ToleranceInterval.jsx
@@ -9,12 +9,14 @@ import SVG from 'svg.js';
 class ToleranceInterval extends React.Component {
 
   drawToleranceInterval(svg, store){
+    const ROULETTE_ORDER = [0, 32, 15, 19, 4, 21, 2, 25, 17, 34, 6, 27, 13, 36, 11, 30, 8, 23, 10, 5, 24, 16, 33, 1, 20, 14, 31, 9, 22, 18, 29, 7, 28, 12, 35, 3, 26];
+
     const outerCircle = svg.querySelector('#SvgjsCircle1082');
-    const cx = outerCircle.getAttribute("cx");
-    const cy = outerCircle.getAttribute("cy");
-    const r = outerCircle.getAttribute("r");
+    const cx = parseFloat(outerCircle.getAttribute("cx"));
+    const cy = parseFloat(outerCircle.getAttribute("cy"));
+    const r = parseFloat(outerCircle.getAttribute("r"));
     const innerCircle = svg.querySelector('#SvgjsCircle1083');
-    const r0 = innerCircle.getAttribute("r");
+    const r0 = parseFloat(innerCircle.getAttribute("r"));
 
     const minProbabilities = store.minProbabilities;
     const maxProbabilities = store.maxProbabilities;
@@ -45,10 +47,73 @@ class ToleranceInterval extends React.Component {
         shift = r - maxHigh * scale;//fix shift
     }
 
-
-
     const draw = SVG(svg);
-    const rect = draw.rect(100, 100);
+
+    //draw max ti
+    var arr = [];
+    var p0;
+    for (var i = 0; i < maxProbabilities.length; i++) {
+        var _rx = shift + maxProbabilities[ROULETTE_ORDER[i]] * scale;
+        var a = 360.0 * i / maxProbabilities.length - 90;
+        var p = [];
+        p.push(cx + _rx * Math.cos(a * (Math.PI / 180)));
+        p.push(cy + _rx * Math.sin(a * (Math.PI / 180)));
+        arr.push(p);
+        if (i == 0) {
+            p0 = p;
+        }
+    }
+    arr.push(p0);
+    draw.polyline(arr).fill({color: 'blue', opacity: 0.05}).stroke({width: 1, color: 'blue'});
+
+    //draw min ti
+    arr.length = 0;
+    var p0;
+    for (var i = 0; i < minProbabilities.length; i++) {
+        var _rx = shift + minProbabilities[ROULETTE_ORDER[i]] * scale;
+        var a = 360.0 * i / minProbabilities.length - 90;
+        var p = [];
+        p.push(cx + _rx * Math.cos(a * (Math.PI / 180)));
+        p.push(cy + _rx * Math.sin(a * (Math.PI / 180)));
+        arr.push(p);
+        if (i == 0) {
+            p0 = p;
+        }
+    }
+    arr.push(p0);
+    draw.polyline(arr).fill({color: 'white'}).stroke({width: 1, color: 'blue'});
+
+    //draw ideal line
+    const rn = shift + n * scale;
+    draw.circle(rn * 2).attr({
+        cx: cx,
+        cy: cy,
+        fill: "none",
+        stroke: 'green',
+        "stroke-width": 0.5
+    });
+
+    //draw zero line
+    const rz = shift + z * scale;
+    draw.circle(rz * 2).attr({
+        cx: cx,
+        cy: cy,
+        fill: "none",
+        stroke: 'red',
+        opacity: 0.5,
+        "stroke-width": 0.5
+    });
+
+    //draw double zero line
+    const rzz = shift + zz * scale;
+    draw.circle(rzz * 2).attr({
+        cx: cx,
+        cy: cy,
+        fill: "none",
+        stroke: 'black',
+        opacity: 0.5,
+        "stroke-width": 0.5
+    });
   }
 
   render() {
--- src/index.js
+++ src/index.js
@@ -20,8 +20,12 @@ render(
   document.getElementById("root")
 );
 
-for (var i = 0; i < 37; i++) {
-  store.addStat(Math.ceil(100 * Math.random()));
+const sampleData = [1004, 1082, 1120, 1030, 1196, 1126, 1072, 1105, 1036, 1049, 1107, 1043, 1083, 1019, 989,1080,
+1005, 1161, 1123, 1073, 1084, 1180, 1114, 1116, 1023, 1160, 1040, 1008, 1075, 1120, 1108, 1098,
+1119, 1033, 1054, 1076, 1087];
+
+for (var i = 0; i < sampleData.length; i++) {
+  store.addStat(sampleData[i]);
 }
 
 // playing around in the console
